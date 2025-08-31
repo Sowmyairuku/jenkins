de047369d0eeb8992405aaf10a6136a1b6363844
Sowmyairuku <163623457+Sowmyairuku@users.noreply.github.com>
2025-08-31 17:28:15 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,19 @@
 public class Factorial {
-    public static long compute(int n){
+    public static long compute(int n) {
         long fact = 1;
-        if (n==1 || n==0) return fact;
-        else{
-         // use long for larger results
-
-        for (int i = 1; i <= num; i++) {
-            fact *= i;
-        }
-            return fact
-
+        if (n == 1 || n == 0) {
+            return fact;
+        } else {
+            // use long for larger results
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
+            }
+            return fact;
         }
     }
+
     public static void main(String[] args) {
         int num = 5; // number to find factorial of
-        
-
-        System.out.println("Factorial of " + num + " is: " + compute(n));
+        System.out.println("Factorial of " + num + " is: " + compute(num));
     }
 }
